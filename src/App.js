8d4a7af1f4b0a2206b967876f9b556482c8ea02e
Jordy Manner <jordy.manner@milkcreation.fr>
2021-09-24 12:10:45 +0200

    feat/v1.0 Adding Creation and Editing Capabilities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import * as React from 'react'
-import { Admin, Resource } from 'react-admin';
-import { PostList } from './posts';
-import { UserList } from './users';
+import {Admin, Resource} from 'react-admin'
+import {PostList, PostEdit, PostCreate} from './posts'
+import {UserList} from './users'
 import jsonServerProvider from 'ra-data-json-server'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 const App = () => (
     <Admin dataProvider={dataProvider}>
-      <Resource name="posts" list={PostList} />
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} options={{label: 'Articles'}}/>
       <Resource name="users" list={UserList}/>
     </Admin>
 )
