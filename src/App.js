e96c0e9e4ae5c454a6d03ac04ca66b001ec43e67
Jordy Manner <jordy.manner@milkcreation.fr>
2021-09-24 15:13:20 +0200

    feat/1.0 Supporting Mobile Devices

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import {Admin, Resource} from 'react-admin'
 import {PostList, PostEdit, PostCreate} from './posts'
 import {UserList} from './users'
 import Dashboard from "./Dashboard";
+import authProvider from './authProvider';
 import jsonServerProvider from 'ra-data-json-server'
 import PostIcon from '@material-ui/icons/Book'
 import UserIcon from '@material-ui/icons/Group'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 const App = () => (
-    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Admin authProvider={authProvider} dashboard={Dashboard} dataProvider={dataProvider}>
       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} options={{label: 'Articles'}} icon={PostIcon}/>
       <Resource name="users" list={UserList} options={{label: 'Utilisateurs'}} icon={UserIcon}/>
     </Admin>
--- src/posts.js
+++ src/posts.js
@@ -1,6 +1,8 @@
-import * as React from "react";
+import * as React from "react"
+import { useMediaQuery } from '@material-ui/core'
 import {
   List,
+  SimpleList,
   Datagrid,
   TextField,
   ReferenceField,
@@ -21,18 +23,30 @@ const postFilters = [
 ];
 
 
-export const PostList = props => (
-    <List filters={postFilters} {...props}>
-      <Datagrid>
-        <TextField source="id"/>
-        <ReferenceField source="userId" reference="users">
-          <TextField source="name"/>
-        </ReferenceField>
-        <TextField source="title"/>
-        <EditButton/>
-      </Datagrid>
-    </List>
-);
+export const PostList = props => {
+  const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+
+  return (
+      <List filters={postFilters} {...props}>
+        {isSmall ? (
+            <SimpleList
+                primaryText={record => record.title}
+                secondaryText={record => `${record.views} views`}
+                tertiaryText={record => new Date().toLocaleDateString()}
+            />
+        ) : (
+        <Datagrid>
+          <TextField source="id"/>
+          <ReferenceField source="userId" reference="users">
+            <TextField source="name"/>
+          </ReferenceField>
+          <TextField source="title"/>
+          <EditButton/>
+        </Datagrid>
+        )}
+      </List>
+  )
+}
 
 const PostTitle = ({record}) => {
   return <span>Article {record ? `"${record.title}"` : ''}</span>;
