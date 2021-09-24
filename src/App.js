8cdf2352d18902c731656e4b2c2677fc1e992ea8
Jordy Manner <jordy.manner@milkcreation.fr>
2021-09-24 14:49:35 +0200

    feat/v1.0 Using a Custom Home Page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import * as React from 'react'
 import {Admin, Resource} from 'react-admin'
 import {PostList, PostEdit, PostCreate} from './posts'
 import {UserList} from './users'
+import Dashboard from "./Dashboard";
 import jsonServerProvider from 'ra-data-json-server'
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
+import PostIcon from '@material-ui/icons/Book'
+import UserIcon from '@material-ui/icons/Group'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} options={{label: 'Articles'}} icon={PostIcon}/>
       <Resource name="users" list={UserList} options={{label: 'Utilisateurs'}} icon={UserIcon}/>
     </Admin>
